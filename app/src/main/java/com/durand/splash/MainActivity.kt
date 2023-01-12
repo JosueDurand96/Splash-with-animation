bbda37b43c559b1728db0d195dfd3062d1758f72
JosueDurand96 <josuedurandf@gmail.com>
2023-01-12 18:54:08 -0500

    splash and animation

--- app/src/main/java/com/durand/splash/MainActivity.kt
+++ app/src/main/java/com/durand/splash/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.durand.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Thread.sleep(2000)
+        screenSplash.setKeepOnScreenCondition{
+            true
+        }
+
+
+        val intent = Intent(this, DetailActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
